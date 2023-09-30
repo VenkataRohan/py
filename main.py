8af8925e6828d932d0f9abc0646e2741f5f97a37
venkatarohan <venkatarohank@gmail.com>
2023-10-01 00:21:55 +0530

    added diffence in excel and json

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-from test import dataframe_unmatched_col
+from test import dataframe_unmatched_col,unmatched_list_domainname_excel_df
 from dotenv import load_dotenv
 import os
 
@@ -19,10 +19,6 @@ recipient_email = 'venkatarohank@gmail.com'
 subject = 'Unmatched columns'
 
 
-
-# Convert the DataFrame to a CSV file
-csv_data = dataframe_unmatched_col.to_csv(index=False)
-
 # Create an email message
 email_message = MIMEMultipart()
 email_message['From'] = sender_email
@@ -30,19 +26,29 @@ email_message['To'] = recipient_email
 email_message['Subject'] = subject
 
 # Attach the CSV data as an attachment
-attachment = MIMEApplication(csv_data.encode('utf-8'))
-attachment.add_header('content-disposition', 'attachment', filename='data.csv')
-email_message.attach(attachment)
+# csv_data = dataframe_unmatched_col.to_csv(index=False)
+# attachment = MIMEApplication(csv_data.encode('utf-8'))
+# attachment.add_header('content-disposition', 'attachment', filename='data.csv')
+# email_message.attach(attachment)
 
-# Connect to the SMTP server
-with smtplib.SMTP(smtp_server, smtp_port) as server:
-    # Start TLS (Transport Layer Security) for security (if using port 587)
-    server.starttls()
+# Attach the CSV data as an attachment
+if not unmatched_list_domainname_excel_df.empty:
+    csv_data2 = unmatched_list_domainname_excel_df.to_csv(index=False)
+    attachment2 = MIMEApplication(csv_data2.encode('utf-8'))
+    attachment2.add_header('content-disposition', 'attachment', filename='Missing_Data_Excel.csv')
+    email_message.attach(attachment2)
+    # Connect to the SMTP server
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        # Start TLS (Transport Layer Security) for security (if using port 587)
+        server.starttls()
 
-    # Log in to the email account
-    server.login(sender_email, sender_password)
+        # Log in to the email account
+        server.login(sender_email, sender_password)
 
-    # Send the email
-    server.sendmail(sender_email, recipient_email, email_message.as_string())
+        # Send the email
+        server.sendmail(sender_email, recipient_email, email_message.as_string())
+
+
+
 
 print('Email with DataFrame attached sent successfully.')
--- test.py
+++ test.py
@@ -2,17 +2,16 @@ import pandas as pd
 import requests
 
 
-excel_file_path = "tst2.xlsx"
+excel_file_path = "tst.xlsx"
 df = pd.read_excel(excel_file_path)
 
 api_url = "http://localhost:5005"
 json_data = requests.get(api_url)
 
-# print(json_data)
+
 if json_data.status_code == 200:
     # Request was successful
     data = json_data.json()  # Assuming the json_data is in JSON format
-    # print(data)
 else:
     # Request failed
     print("API request failed with status code:", json_data.status_code)
@@ -20,21 +19,40 @@ else:
 
 
 unmatched_list=[]
+unmatched_list_domainname_json=[]
+unmatched_list_domainname_excel=[]
+excel_ids=[]
 
 keys=data[0].keys()
 for index, excel_row in df.iterrows():
     # Assuming 'ID' is a unique identifier in your Excel data
     excel_id = excel_row['ID']
-
-    
-    
+    excel_ids.append(excel_id)
     json_entry = next((entry for entry in data if entry.get('ID') == excel_id), None)
+     
     unmatched_obj={}
-    for val in keys:
-        if excel_row[val] != json_entry[val]:
-            # unmatched_obj[val]={"previous_val":excel_row[val], "updated_val": json_entry[val]}
-            unmatched_list.append({"ID":excel_row['ID'],"Column Name":val,"Previous Value":excel_row[val],"Updated Value":json_entry[val]})
-            df.at[index,val]=json_entry[val]  
+    if json_entry:
+        for val in keys:
+            if excel_row[val] != json_entry[val]:
+                # unmatched_obj[val]={"previous_val":excel_row[val], "updated_val": json_entry[val]}
+                unmatched_list.append({"ID":excel_row['ID'],"Column Name":val,"Previous Value":excel_row[val],"Updated Value":json_entry[val]})
+                df.at[index,val]=json_entry[val]  
+    else:
+        unmatched_list_domainname_json.append({"ID":excel_row['ID'],"Domain Name":excel_row['Domain Name']})
+
+
+#checking missing data in excel 
+for entry in data:
+    temp=[]
+    if  entry.get("ID") not in excel_ids:
+        unmatched_list_domainname_excel.append({"ID":entry.get('ID'),"Domain Name":entry.get('Domain Name')})
+        for key in keys:
+            temp.append(entry.get(key))
+        df.loc[len(df.index)] = temp   
+
+
+
+# df.loc[len(df.index)] = ['Amy', 89, 93] 
 
 print("Data Frame that is saved in excel")
 print(df)
@@ -43,7 +61,19 @@ print(df)
 df.to_excel(excel_file_path, index=False)
 
 dataframe_unmatched_col= pd.DataFrame(unmatched_list)
+# print()
+# print("Data Frame send in email")
+# print(dataframe_unmatched_col)
+
+
+unmatched_list_domainname_excel_df= pd.DataFrame(unmatched_list_domainname_excel)
 print()
-print("Data Frame send in email")
-print(dataframe_unmatched_col)
+print("Values present in json missing in excel")
+print(unmatched_list_domainname_excel_df)
+
+
+unmatched_list_domainname_json_df= pd.DataFrame(unmatched_list_domainname_json)
+print()
+print("Values present in excel missing in json")
+print(unmatched_list_domainname_json_df)
 
